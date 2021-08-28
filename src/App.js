9aec6c39f55b6aeaeaf4cc99b94d32edec3179f3
seprator <seprator.hd@gmail.com>
2021-08-28 15:41:21 +0300

    this is for learning tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 import { v4 as uuid } from "uuid";
 import { useState } from "react";
 
@@ -45,104 +45,118 @@ function App() {
 
   return (
     <>
-      <h1>Calculate CBM</h1>
-      <article className="form">
-        <form className="form-control">
-          <label htmlFor="productName">product:</label>
-          <input
-            id="productName"
-            type="text"
-            value={info.productName}
-            placeholder="cm"
-            name="productName"
-            onChange={handleChange}
-          />
-          <label htmlFor="hight">Hight:</label>
-          <input
-            id="hight"
-            type="number"
-            value={info.hight}
-            placeholder="cm"
-            name="hight"
-            onChange={handleChange}
-          />
-          <label htmlFor="length">Length:</label>
-          <input
-            id="length"
-            type="number"
-            value={info.length}
-            placeholder="cm"
-            name="length"
-            onChange={handleChange}
-          />
-          <label htmlFor="width">Width:</label>
-          <input
-            id="width"
-            type="number"
-            value={info.width}
-            placeholder="cm"
-            name="width"
-            onChange={handleChange}
-          />
-          <label htmlFor="quantity">Quantity:</label>
-          <input
-            id="quantity"
-            type="number"
-            value={info.quantity}
-            placeholder="number of items or boxs"
-            name="quantity"
-            onChange={handleChange}
-          />
-          <label htmlFor="shipPrice">Shipping price for one CMB:</label>
-          <input
-            id="shipPrice"
-            type="number"
-            value={info.shipPrice}
-            placeholder="$"
-            name="shipPrice"
-            onChange={handleChange}
-          />{" "}
-          <label htmlFor="productPrice">Price of product:</label>
-          <input
-            id="productPrice"
-            type="number"
-            value={info.productPrice}
-            placeholder="$"
-            name="productPrice"
-            onChange={handleChange}
-          />
-          <button type="submit" className="btn" onClick={handleSubmit}>
-            Calculate
-          </button>
-        </form>
-      </article>
-      <article>
-        {display.map((info) => {
-          const {
-            id,
-            length,
-            hight,
-            width,
-            shipPrice,
-            quantity,
-            productPrice,
-          } = info;
-          let cmb_el =
-            (info.width * info.hight * info.length * info.quantity) / 1000000;
-          let shipPrice_el = cmb_el * info.shipPrice;
-          let productPrice_el = info.quantity * info.productPrice;
-          let total_el = productPrice_el * 1 + shipPrice_el;
-          return (
-            <div key={id} className="item">
-              <h4>Name: {info.productName}</h4>
-              <h4>CMB: {cmb_el} $</h4>
-              <h4>Shipping{shipPrice_el} $</h4>
-              <h4>Products: {productPrice_el} $</h4>
-              <h4>Total: {total_el} $</h4>
-            </div>
-          );
-        })}
-      </article>
+      <div className="">
+        <h1 className="mx-auto flex justify-center">Calculate CBM</h1>
+        <article className="">
+          <form className="bg-white container flex-col flex max-w-lg align-middle justify-center mx-auto border-1 shadow-md">
+            <label className="bg-gray-100" htmlFor="productName">
+              product:
+            </label>
+            <input
+              className="bg-gray-700"
+              id="productName"
+              type="text"
+              value={info.productName}
+              placeholder="name"
+              name="productName"
+              onChange={handleChange}
+            />
+            <label htmlFor="hight">Hight:</label>
+            <input
+              className="bg-gray-700"
+              id="hight"
+              type="number"
+              value={info.hight}
+              placeholder="cm"
+              name="hight"
+              onChange={handleChange}
+            />
+            <label htmlFor="length">Length:</label>
+            <input
+              className="bg-gray-700"
+              id="length"
+              type="number"
+              value={info.length}
+              placeholder="cm"
+              name="length"
+              onChange={handleChange}
+            />
+            <label htmlFor="width">Width:</label>
+            <input
+              className="bg-gray-700"
+              id="width"
+              type="number"
+              value={info.width}
+              placeholder="cm"
+              name="width"
+              onChange={handleChange}
+            />
+            <label htmlFor="quantity">Quantity:</label>
+            <input
+              className="bg-gray-700"
+              id="quantity"
+              type="number"
+              value={info.quantity}
+              placeholder="number of items or boxs"
+              name="quantity"
+              onChange={handleChange}
+            />
+            <label htmlFor="shipPrice">Shipping price for one CMB:</label>
+            <input
+              className="bg-gray-700"
+              id="shipPrice"
+              type="number"
+              value={info.shipPrice}
+              placeholder="$"
+              name="shipPrice"
+              onChange={handleChange}
+            />{" "}
+            <label htmlFor="productPrice">Price of product:</label>
+            <input
+              className="bg-gray-700"
+              id="productPrice"
+              type="number"
+              value={info.productPrice}
+              placeholder="$"
+              name="productPrice"
+              onChange={handleChange}
+            />
+            <button type="submit" className="" onClick={handleSubmit}>
+              Calculate
+            </button>
+          </form>
+        </article>
+        <article>
+          {display.map((info) => {
+            const {
+              id,
+              length,
+              hight,
+              width,
+              shipPrice,
+              quantity,
+              productPrice,
+            } = info;
+            let cmb_el =
+              (info.width * info.hight * info.length * info.quantity) / 1000000;
+            let shipPrice_el = cmb_el * info.shipPrice;
+            let productPrice_el = info.quantity * info.productPrice;
+            let total_el = productPrice_el * 1 + shipPrice_el;
+            return (
+              <div
+                key={id}
+                className="flex flex-col mx-auto justify-items-center  bg-gray-200 max-w-lg border-3 shadow-md my-1 px-1 flex-auto "
+              >
+                <h4>Name: {info.productName}</h4>
+                <h4>CMB: {cmb_el} m</h4>
+                <h4>Shipping: {shipPrice_el} $</h4>
+                <h4>Products: {productPrice_el} $</h4>
+                <h4>Total: {total_el} $</h4>
+              </div>
+            );
+          })}
+        </article>
+      </div>
     </>
   );
 }
